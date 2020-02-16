400b7c7b7c7d500eeb5acaef8081ad40a0a670e2
Ануар Кабылбаев <kabylbaevanuar@gmail.com>
2020-02-16 11:25:59 +0600

    init project

---
+++ web parsing (with webBrowser and webClient)/web parsing (with webBrowser and webClient)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace web_parsing__with_webBrowser_and_webClient_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
