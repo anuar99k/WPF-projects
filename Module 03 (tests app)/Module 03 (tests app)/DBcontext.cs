c9804e2ba0aea19b7544219b90e292a63fabcc1f
Ануар Кабылбаев <kabylbaevanuar@gmail.com>
2020-02-29 21:03:58 +0600

    db context (liteDB)

---
+++ Module 03 (tests app)/Module 03 (tests app)/DBcontext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace Module_03__tests_app_
+{
+    class DBcontext
+    {
+        public List<User> GetAllUsers()
+        {
+            List<User> users = new List<User>();
+
+            using (var db = new LiteDatabase(@"localDb.db"))
+            {
+                users = db.GetCollection<User>("user").FindAll().ToList();
+            }
+            return users;
+        }
+
+        public bool AddUser(User user)
+        {
+            using (var db = new LiteDatabase(@"localDb.db"))
+            {
+                var users = db.GetCollection<User>("user");
+
+                users.Insert(user);
+                return true;
+            }
+        }
+
+        public bool AddUser(string login, string password)
+        {
+            User user = new User() { Login = login, Password = password };
+
+            using(var db = new LiteDatabase(@"localDb.db"))
+            {
+                var users = db.GetCollection<User>("user");
+
+                users.Insert(user);
+                return true;
+            }
+        }
+    }
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
