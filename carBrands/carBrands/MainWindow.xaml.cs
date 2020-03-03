b3484b6b4f435be51fa9966bd92f7682820924ae
Ануар Кабылбаев <kabylbaevanuar@gmail.com>
2020-03-03 19:28:49 +0600

    adding buttons

--- carBrands/carBrands/MainWindow.xaml.cs
+++ carBrands/carBrands/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,30 @@ namespace carBrands
         public MainWindow()
         {
             InitializeComponent();
+
+            loadButtons();
+        }
+
+        private void loadButtons()
+        {
+            string[] carBrands = Directory.GetFiles(@"Images", "*.png");
+            foreach(string carBrand in carBrands)
+            {
+                Button btn = new Button() { Background = Brushes.Transparent, BorderThickness = new Thickness(0, 0, 0, 0) };
+                StackPanel spBtnContent = new StackPanel() { Orientation = Orientation.Horizontal };
+                Image img = new Image() { Width = 50, Height = 50 };
+                img.Source = new BitmapImage(new Uri(carBrand, UriKind.Relative));
+                TextBlock textOfBtn = new TextBlock();
+                textOfBtn.VerticalAlignment = VerticalAlignment.Center;
+                //textOfBtn.TextDecorations = 
+                textOfBtn.Padding = new Thickness(5, 5, 5, 5);
+                textOfBtn.FontSize = 16;
+                textOfBtn.Text = carBrand.Split('\\')[1].Split('.')[0];
+                spBtnContent.Children.Add(img);
+                spBtnContent.Children.Add(textOfBtn);
+                btn.Content = spBtnContent;
+                buttonsWrapper.Children.Add(btn);
+            }
         }
     }
 }
