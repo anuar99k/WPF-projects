4cd853b691f068420b378656abeb80a1eb2b7e1f
Ануар Кабылбаев <kabylbaevanuar@gmail.com>
2020-03-10 22:43:25 +0600

    creating and binding data grid

--- Module 05 (working with sql server)/Module 05 (working with sql server)/MainWindow.xaml.cs
+++ Module 05 (working with sql server)/Module 05 (working with sql server)/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Module_05__working_with_sql_server_.Pages;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Module_05__working_with_sql_server_
 {
@@ -23,6 +11,21 @@ namespace Module_05__working_with_sql_server_
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MiEquipment_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new MainFormPage("equipment"));
+        }
+
+        private void MiManufacture_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new MainFormPage("manufacture"));
+        }
+
+        private void MiModel_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new MainFormPage("model"));
         }
     }
 }
---
+++ Module 05 (working with sql server)/Module 05 (working with sql server)/Pages/MainFormPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Module_05_.BLL;
+using Module_05_.DAL.Models;
+using System.Windows.Controls;
+
+namespace Module_05__working_with_sql_server_.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainFormPage.xaml
+    /// </summary>
+    public partial class MainFormPage : Page
+    {
+        public MainFormPage(string menuItemName)
+        {
+            InitializeComponent();
+
+            switch (menuItemName)
+            {
+                case "equipment":
+                    dataGrid.ItemsSource = DbLogic<Equipment>.GetAll();
+                    break;
+                case "manufacture":
+                    dataGrid.ItemsSource = DbLogic<Manufacturer>.GetAll();
+                    break;
+                case "model":
+                    dataGrid.ItemsSource = DbLogic<Model>.GetAll();
+                    break;
+            }
+        }
+    }
+}
