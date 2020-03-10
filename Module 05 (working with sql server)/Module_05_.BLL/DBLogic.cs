85d301709ce38a007c6a5c675eb2d33c1af54c1c
Ануар Кабылбаев <kabylbaevanuar@gmail.com>
2020-03-10 09:28:50 +0600

    business logic level

---
+++ Module 05 (working with sql server)/Module_05_.BLL/DBLogic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module_05_.DAL;
+
+namespace Module_05_.BLL
+{
+    class DBLogic<T> where T : class
+    {
+        public static List<T> GetAll(string tableName)
+        {
+            return DbContext<T>.GetAll(tableName);
+        }
+
+        public static T GetById(string tableName, int id)
+        {
+            return DbContext<T>.GetById(tableName, id);
+        }
+
+        public static bool Insert(string tableName, T data)
+        {
+            if (DbContext<T>.Insert(tableName, data))
+                return true;
+            else
+                return false;
+        }
+
+        public static bool Update(string tableName, T data)
+        {
+            if (DbContext<T>.Update(tableName, data))
+                return true;
+            else
+                return false;
+        }
+
+        public static bool Delete(string tableName, int id)
+        {
+            if (DbContext<T>.Delete(tableName, id))
+                return true;
+            else
+                return false;
+        }
+    }
+}
