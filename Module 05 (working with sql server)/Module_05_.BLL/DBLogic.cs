d22a83045742e1c07ee447b0ca184bfcd9e739a1
Ануар Кабылбаев <kabylbaevanuar@gmail.com>
2020-03-10 22:40:04 +0600

    updating business logic level

--- Module 05 (working with sql server)/Module_05_.BLL/DBLogic.cs
+++ Module 05 (working with sql server)/Module_05_.BLL/DBLogic.cs
@@ -1,42 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Module_05_.DAL;
+using Module_05_.DAL.Models;
 
 namespace Module_05_.BLL
 {
-    class DBLogic<T> where T : class
+    public class DbLogic<T> where T : class
     {
-        public static List<T> GetAll(string tableName)
+        public static List<T> GetAll()
         {
+            string tableName = string.Empty;
+            switch (typeof(T).Name)
+            {
+                case "Equipment":
+                    tableName = "newEquipment";
+                    break;
+                case "Manufacturer":
+                    tableName = "TablesManufacturer";
+                    break;
+                case "Model":
+                    tableName = "TablesModel";
+                    break;
+            }
             return DbContext<T>.GetAll(tableName);
         }
 
-        public static T GetById(string tableName, int id)
+        public static T GetById(int id)
         {
+            string tableName = string.Empty;
+            switch (typeof(T).Name)
+            {
+                case "Equipment":
+                    tableName = "newEquipment";
+                    break;
+                case "Manufacturer":
+                    tableName = "TablesManufacturer";
+                    break;
+                case "Model":
+                    tableName = "TablesModel";
+                    break;
+            }
             return DbContext<T>.GetById(tableName, id);
         }
 
-        public static bool Insert(string tableName, T data)
+        public static bool Insert(T data)
         {
+            string tableName = string.Empty;
+            switch (typeof(T).Name)
+            {
+                case "Equipment":
+                    tableName = "newEquipment";
+                    break;
+                case "Manufacturer":
+                    tableName = "TablesManufacturer";
+                    break;
+                case "Model":
+                    tableName = "TablesModel";
+                    break;
+            }
             if (DbContext<T>.Insert(tableName, data))
                 return true;
             else
                 return false;
         }
 
-        public static bool Update(string tableName, T data)
+        public static bool Update(T data)
         {
+            string tableName = string.Empty;
+            switch (typeof(T).Name)
+            {
+                case "Equipment":
+                    tableName = "newEquipment";
+                    break;
+                case "Manufacturer":
+                    tableName = "TablesManufacturer";
+                    break;
+                case "Model":
+                    tableName = "TablesModel";
+                    break;
+            }
             if (DbContext<T>.Update(tableName, data))
                 return true;
             else
                 return false;
         }
 
-        public static bool Delete(string tableName, int id)
+        public static bool Delete(T data)
         {
+            string tableName = string.Empty;
+            int id = -1;
+            switch (typeof(T).Name)
+            {
+                case "Equipment":
+                    tableName = "newEquipment";
+                    id = (data as Equipment).intEquipmentID;
+                    break;
+                case "Manufacturer":
+                    tableName = "TablesManufacturer";
+                    id = (data as Manufacturer).intManufacturerID;
+                    break;
+                case "Model":
+                    tableName = "TablesModel";
+                    id = (data as Model).intModelID;
+                    break;
+            }
             if (DbContext<T>.Delete(tableName, id))
                 return true;
             else
